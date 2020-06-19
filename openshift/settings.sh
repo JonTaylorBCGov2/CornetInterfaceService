73b75867c4800b3af00a4e88859f9d784d9e6231
JonTaylorBCGov2 <55515424+JonTaylorBCGov2@users.noreply.github.com>
2020-06-19 14:18:25 -0700

    Update settings.sh

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -6,18 +6,18 @@ export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
 export skip_git_overrides=""
-export GIT_URI="https://github.com/JonTaylorBCGov/CASInterfaceService.git"
+export GIT_URI="https://github.com/JonTaylorBCGov/CornetInterfaceService.git"
 export GIT_REF="master"
 
 # The project components
 # - defaults to the support the Simple Project Structure
-export components=${components:-"cas-interface-service"}
+export components=${components:-"cornet-interface-service"}
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
 export builds=${builds:-""}
 
 # The images to be tagged after build
-export images=${images:-"cas-interface-service"}
+export images=${images:-"cornet-interface-service"}
 
 # The routes for the project
 export routes=${routes:-""}
